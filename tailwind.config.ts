06ff4b0cbbc20e2f1ce085729a3fc513e90a1f4e
Garcia Sébastien <34031275+Sebestala@users.noreply.github.com>
2024-10-07 19:47:54 +0200

    new boilerplate without turborepo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,9 +12,19 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "var(--primary)",
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+        },
+        selected: {
+          DEFAULT: "var(--selected)",
+          foreground: "var(--selected-foreground)",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
