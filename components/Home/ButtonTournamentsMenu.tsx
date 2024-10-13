c0dcb8e8305c66ea799284645e5cdbf35ea18deb
Garcia Sébastien <34031275+Sebestala@users.noreply.github.com>
2024-10-13 11:38:31 +0200

    TournamentsMenu created

--- components/Home/ButtonTournamentsMenu.tsx
+++ components/Home/ButtonTournamentsMenu.tsx
@@ -2,46 +2,12 @@
 
 import React, { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { X } from "lucide-react";
-import { useBudget } from "@/context/BudgetContext";
 import { PokerChip } from "../ui/PokerChip";
-import { PokerChipToggle } from "./PokerChipToggle";
-import { useForm } from "react-hook-form";
-
-interface BudgetFormData {
-  minBudget: number;
-  maxBudget: number;
-}
+import { TournamentsMenu } from "./TounamentsMenu";
 
 export function ButtonTournamentsMenu() {
   const [isPanelOpen, setIsPanelOpen] = useState(false);
   const [isDesktop, setIsDesktop] = useState(false);
-  const {
-    minBudget,
-    maxBudget,
-    setMinBudget,
-    setMaxBudget,
-    isTripleTournaments,
-    setIsTripleTournaments,
-    toggleIsChangeBudget,
-  } = useBudget();
-
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-    trigger,
-  } = useForm<BudgetFormData>({
-    defaultValues: {
-      minBudget: minBudget,
-      maxBudget: maxBudget,
-    },
-    mode: "onChange",
-  });
-
-  const watchMinBudget = watch("minBudget");
-  const watchMaxBudget = watch("maxBudget");
 
   useEffect(() => {
     const handleResize = () => {
@@ -57,13 +23,6 @@ export function ButtonTournamentsMenu() {
   }, []);
 
   const togglePanel = () => setIsPanelOpen(!isPanelOpen);
-
-  const onValidSubmit = (data: BudgetFormData) => {
-    setMinBudget(data.minBudget);
-    setMaxBudget(data.maxBudget);
-    toggleIsChangeBudget();
-    togglePanel();
-  };
 
   return (
     <div className="relative">
@@ -96,107 +55,7 @@ export function ButtonTournamentsMenu() {
       )}
 
       <AnimatePresence>
-        {isPanelOpen && (
-          <div className="max-w-screen-md w-full mx-auto fixed inset-x-0 bottom-0">
-            <motion.div
-              initial={{ y: "100%" }}
-              animate={{ y: 0 }}
-              exit={{ y: "100%" }}
-              transition={{ type: "spring", stiffness: 300, damping: 30 }}
-              className="bg-white shadow-lg rounded-t-2xl p-6 z-40 pb-20 mx-2 lg:mx-0 lg:-ml-3"
-            >
-              <button
-                onClick={togglePanel}
-                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
-              >
-                <X size={24} />
-              </button>
-              <div className="flex items-center space-x-4 mb-4">
-                <h2 className="text-3xl font-archivoBold text-textColor">Triple Tournois</h2>
-                <PokerChipToggle onChange={() => setIsTripleTournaments(!isTripleTournaments)} />
-              </div>
-              <p className="text-sm text-textColor font-archivo mb-4">
-                Entrez votre fourchette de budget pour trouver les triplets de tournois parfaits
-                pour vous !
-              </p>
-              <form
-                onSubmit={handleSubmit(onValidSubmit)}
-                className="space-y-4"
-              >
-                <div className="flex items-center space-x-4">
-                  <div className="w-1/2">
-                    <label
-                      htmlFor="minBudget"
-                      className="block text-sm font-medium text-textColor font-archivoMedium mb-1"
-                    >
-                      Budget Minimum (€)
-                    </label>
-                    <input
-                      type="number"
-                      id="minBudget"
-                      {...register("minBudget", {
-                        required: "Ce champ est requis",
-                        min: { value: 1, message: "Le budget minimum doit être d'au moins 1€" },
-                        max: {
-                          value: 10000,
-                          message: "Le budget minimum ne peut pas dépasser 10 000€",
-                        },
-                        validate: (value) =>
-                          Number(value) <= Number(watchMaxBudget) ||
-                          "Le budget minimum ne peut pas être supérieur au budget maximum",
-                      })}
-                      className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                      placeholder="1 €"
-                      onChange={() => {
-                        trigger("maxBudget");
-                      }}
-                    />
-                  </div>
-                  <div className="w-1/2">
-                    <label
-                      htmlFor="maxBudget"
-                      className="block text-sm font-medium text-textColor mb-1 font-archivoMedium"
-                    >
-                      Budget Maximum (€)
-                    </label>
-                    <input
-                      type="number"
-                      id="maxBudget"
-                      {...register("maxBudget", {
-                        required: "Ce champ est requis",
-                        min: { value: 1, message: "Le budget maximum doit être d'au moins 1€" },
-                        max: {
-                          value: 10000,
-                          message: "Le budget maximum ne peut pas dépasser 10 000€",
-                        },
-                        validate: (value) =>
-                          Number(value) >= Number(watchMinBudget) ||
-                          "Le budget maximum ne peut pas être inférieur au budget minimum",
-                      })}
-                      className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                      placeholder="10 000 €"
-                      onChange={() => {
-                        trigger("minBudget");
-                      }}
-                    />
-                  </div>
-                </div>
-                {errors.minBudget && (
-                  <p className="text-red-500 text-xs mt-1">{errors.minBudget.message}</p>
-                )}
-                {errors.maxBudget && (
-                  <p className="text-red-500 text-xs mt-1">{errors.maxBudget.message}</p>
-                )}
-                <button
-                  type="submit"
-                  className="w-full bg-primary text-white py-2 px-4 rounded-lg hover:bg-primary/90 font-archivoSemiBold"
-                >
-                  Trouver les Tournois
-                </button>
-              </form>
-            </motion.div>
-          </div>
-        )}
+        {isPanelOpen && <TournamentsMenu togglePanel={togglePanel} />}
       </AnimatePresence>
     </div>
   );
